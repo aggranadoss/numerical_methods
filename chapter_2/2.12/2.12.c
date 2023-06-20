64321b5ad69c13e52dcbe8ce8b98bc6b49dd6900
Andrés López <37550831+lopezandres9512@users.noreply.github.com>
2023-06-20 14:36:36 -0500

    Add files via upload

---
+++ chapter_2/2.12/2.12.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void metodo_burbuja(int *arr, int n);
+
+int main() {
+    int n = 10; // Tamaño del arreglo
+    int arr[n];
+    
+    // arreglo
+    srand(time(NULL)); // números aleatorios
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100; // Generar un número aleatorio entre 0 y 99
+    }
+    
+    // arreglo sin ordenar
+    printf("Arreglo antes de ordenar:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    // método de la burbuja
+    metodo_burbuja(arr, n);
+    
+    // arreglo ordenado
+    printf("Arreglo ordenado:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    return 0;
+}
+
+void metodo_burbuja(int *arr, int n) {
+    int temp;
+    int i = 0;
+    int j = 0;
+    int intercambio = 0;
+    
+    do {
+        intercambio = 0;
+        i = 0;
+        while (i < n-1) {
+            if (arr[i] > arr[i+1]) {
+                // Intercambiar los elementos
+                temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                intercambio = 1;
+            }
+            i++;
+        }
+        n--;
+    } while (intercambio);
+}
+
